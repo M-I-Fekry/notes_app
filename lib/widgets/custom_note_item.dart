e72fe940a642e37a901198b671cdfdb12f377f9e
Fekry <me031230@gmail.com>
2026-01-31 14:29:46 +0200

    feat: add custom note item widget

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top:16,bottom: 16 ),
+              child: Text(
+                'Build your career with M_I_Fekry',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.6),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Icon(FontAwesomeIcons.trash, color: Colors.black),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'January20 ,2006',
+              style: TextStyle(color: Colors.black.withOpacity(.6),fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
