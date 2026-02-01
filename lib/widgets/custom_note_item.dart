fe12c3a591f9ffa7b051ae6d9063390c35c1c17d
Fekry <me031230@gmail.com>
2026-02-01 14:10:50 +0200

    feat: create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,52 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top:16,bottom: 16 ),
-              child: Text(
-                'Build your career with M_I_Fekry',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.6),
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your career with M_I_Fekry',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.6),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Icon(FontAwesomeIcons.trash, color: Colors.black),
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Icon(FontAwesomeIcons.trash, color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'January20 ,2006',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.6),
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'January20 ,2006',
-              style: TextStyle(color: Colors.black.withOpacity(.6),fontSize: 16),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
