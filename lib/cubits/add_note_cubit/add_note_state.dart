1cbfc1631087b6417fe3a8289c09410e5b7c3d46
Fekry <me031230@gmail.com>
2026-02-02 15:11:20 +0200

    feat: add states for add note cubit

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,6 +1,13 @@
 part of 'add_note_cubit.dart';
 
 @immutable
-abstract class NotesState {}
+abstract class AddNoteState {}
 
-class NotesInitial extends NotesState {}
+class AddNotesInitial extends AddNoteState {}
+class AddNotesLoading extends AddNoteState {}
+class AddNotesSuccess extends AddNoteState {}
+class AddNotesFailure extends AddNoteState {
+  final String errMessage;
+AddNotesFailure(this.errMessage);
+}
+
