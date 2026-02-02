27e54ee824c117af9fc94425b5c2effb05145f42
Fekry <me031230@gmail.com>
2026-02-02 16:34:06 +0200

    feat: register NoteModel adapter in Hive

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class NotesCubit extends Cubit<AddNoteState> {
   NotesCubit() : super(AddNotesInitial());
+
+  addNote(NoteModel note)
+  {}
 }
