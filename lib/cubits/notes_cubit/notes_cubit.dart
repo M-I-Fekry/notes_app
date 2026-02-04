2e0a5331c8041c3f39a84eeaaee48fed589e11ab
Fekry <me031230@gmail.com>
2026-02-04 14:35:02 +0200

    feat: add NotesCubit

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/constants.dart';
+
+part 'notes_cubit_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+}
